055f915c68b3a8fcc71158a4a63af32b6a48b708
gallon_shih <gallon_shih@pegatroncorp.com>
2025-07-17 15:39:55 +0800

    Refactor hourly sales summary update to use external SQL file for query; replace dynamic hours in query

--- plugins/services/sales_data_service.py
+++ plugins/services/sales_data_service.py
@@ -1,6 +1,7 @@
 import random
 import logging
 from datetime import datetime, timedelta
+from pathlib import Path
 
 
 class SalesDataService:
@@ -45,19 +46,15 @@ class SalesDataService:
             self.logger.info(f"Starting to update hourly sales summary for the past {hours_ago} hours")
             with self.pg_engine.connect() as con:
                 # Upsert summary data
-                upsert_query = f"""
-                    INSERT INTO retail.hourly_sales_summary (date, hour, total_sales)
-                    SELECT 
-                        DATE(transaction_time) AS date,
-                        EXTRACT(HOUR FROM transaction_time) AS hour,
-                        SUM(total_amount) AS total_sales
-                    FROM retail.sales
-                    WHERE transaction_time >= NOW() - INTERVAL '{hours_ago} HOURS'
-                    GROUP BY date, hour
-                    ON CONFLICT (date, hour) 
-                    DO UPDATE SET total_sales = EXCLUDED.total_sales
-                    WHERE hourly_sales_summary.total_sales <> EXCLUDED.total_sales
-                """
+                # Locate and read the SQL file dynamically
+                sql_file_path = Path(__file__).resolve().parent.parent / 'sql' / 'update_hourly_sales_summary.sql'
+                with sql_file_path.open('r', encoding='utf-8') as file:
+                    upsert_query = file.read()
+
+                # replace __HOURS_AGO__ with the actual value
+                upsert_query = upsert_query.replace('__HOURS_AGO__', str(hours_ago))
+                self.logger.info(f"Getting upsert query for hourly sales summary: {upsert_query}")
+
                 con.execute(upsert_query)
                 self.logger.info(f"Successfully updated hourly sales summary for the past {hours_ago} hours")
         except Exception as e:
